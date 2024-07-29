12f1d62f2a967ca54e5400f900539f3f54760a41
Anvesha Srivastava <137292899+anveshas@users.noreply.github.com>
2024-07-29 21:56:59 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,283 @@
+// // import {
+// //   mobile,
+// //   backend,
+// //   creator,
+// //   web,
+// //   javascript,
+// //   typescript,
+// //   html,
+// //   css,
+// //   reactjs,
+// //   redux,
+// //   tailwind,
+// //   nodejs,
+// //   mongodb,
+// //   git,
+// //   figma,
+// //   docker,
+// //   meta,
+// //   starbucks,
+// //   tesla,
+// //   shopify,
+// //   carrent,
+// //   jobit,
+// //   tripguide,
+// //   threejs,
+// //   // event,
+// //   // C++,
+// //   // event,
+// // } from "../assets";
+
+// // export const navLinks = [
+// //   {
+// //     id: "about",
+// //     title: "About",
+// //   },
+// //   {
+// //     id: "work",
+// //     title: "Work",
+// //   },
+// //   {
+// //     id: "contact",
+// //     title: "Contact",
+// //   },
+// // ];
+
+// // const services = [
+// //   // {
+// //   //   title: "Web Developer",
+// //   //   icon: web,
+// //   // },
+// //   // // {
+// //   // //   title: "React Native Developer",
+// //   // //   icon: mobile,
+// //   // // },
+// //   // {
+// //   //   title: "Frontend Developer",
+// //   //   icon: backend,
+// //   // },
+// //   // {
+// //   //   title: "Problem Solver",
+// //   //   icon: creator,
+// //   // },
+// // ];
+
+// // const experiences = [
+// //   {
+// //     title: "Social Media and Content Head",
+// //     company_name: "Parola: Literary Hub of JIIT",
+// //     icon: starbucks,
+// //     iconBg: "#383E56",
+// //     date: "June 2024 - Present",
+// //     points: [
+// //       "Authored multiple content pieces for the official page of Parola, increasing engagement.",
+// //       "Will be organizing Joust'24, the largest fest at our college, coordinating multiple events and activities.",
+// //     ],
+// //   },
+// //   {
+// //     title: "Marketing Volunteer",
+// //     company_name: "IEEE SB JIIT",
+// //     icon: starbucks,
+// //     iconBg: "#383E56",
+// //     date: "June 2024 - Present",
+// //     points: [
+// //       "Organized activities in Xenith’24, the flagship event of IEEE, with 1000+ students, and actively contributed to designing the Xenith website.",
+// //       "Mentored 200+ students during TechBlocks 9.1, a comprehensive five-day workshop, teaching web development.",
+// //     ],
+// //   },
+// // ];
+
+// // const technologies = [
+// //   {
+// //     name: "HTML 5",
+// //     icon: html,
+// //   },
+// //   {
+// //     name: "CSS 3",
+// //     icon: css,
+// //   },
+// //   {
+// //     name: "JavaScript",
+// //     icon: javascript,
+// //   },
+// //   {
+// //     name: "TypeScript",
+// //     icon: typescript,
+// //   },
+// //   {
+// //     name: "React JS",
+// //     icon: reactjs,
+// //   },
+// //   {
+// //     name: "Tailwind CSS",
+// //     icon: tailwind,
+// //   },
+// //   {
+// //     name: "Node JS",
+// //     icon: nodejs,
+// //   },
+// //   {
+// //     name: "MongoDB",
+// //     icon: mongodb,
+// //   },
+// //   {
+// //     name: "Three JS",
+// //     icon: threejs,
+// //   },
+// //   {
+// //     name: "git",
+// //     icon: git,
+// //   },
+// //   {
+// //     name: "C++",
+// //     icon: docker,
+// //   },
+// // ];
+
+// // const education = [
+// //   {
+// //     title: "BTech, Computer Science",
+// //     company_name: "Jaypee Institute of Information Technology",
+// //     icon: starbucks,
+// //     iconBg: "#383E56",
+// //     date: "September 2022 - May 2026",
+// //     points: [
+// //       "CourseWorks: Data Structures and Algorithms, Operating Systems, Database Management Systems",
+// //       "Societies: Parola: Literary Hub of JIIT and IEEE SB JIIT",
+// //       "Authored content for Parola, organizing Joust'24 and in IEEE, organized activities in Xenith’24, contributed to the Xenith website, and mentored students during TechBlocks 9.1.",
+// //     ],
+// //   },
+// //   {
+// //     title: "Class XII & Class X, CBSE",
+// //     company_name: "Disha Delphi Public School",
+// //     icon: tesla,
+// //     iconBg: "#383E56",
+// //     date: "April 2018 - March 2022",
+// //     points: [
+// //       "Class X - 91%",
+// //       "Class XII - 90%",
+// //       "I have participated in multiple competitions, excelling in sports such as Badminton and Swimming, as well as in debates. ",
+// //     ],
+// //   },
+// // ];
+
+// // const testimonials = [
+// //   {
+// //     testimonial:
+// //       "I have never seen a person this hardworking and deciplined.",
+// //     name: "Vanshika Gupta",
+// //     designation: "Room Mate",
+// //     company: "Anvesha",
+// //     image: "https://randomuser.me/api/portraits/women/4.jpg",
+// //   },
+// // ];
+
+// // const projects = [
+// //   {
+// //     name: "RealEstate",
+// //     description:
+// //       "Web-based platform about renting, buying, or selling your property to another person without the involvement of a third party. It includes a chat functionality to ensure proper communication between the seller or renter and the buyer.",
+// //     tags: [
+// //       {
+// //         name: "MERN",
+// //         color: "blue-text-gradient",
+// //       },
+// //       {
+// //         name: " Socket.io",
+// //         color: "green-text-gradient",
+// //       },
+// //       {
+// //         name: "Axios",
+// //         color: "pink-text-gradient",
+// //       },
+// //     ],
+// //     image: carrent,
+// //     source_code_link: "https://github.com/anveshas/estateapp",
+// //   },
+// //   {
+// //     name: "Stock Market Prediction",
+// //     description:
+// //       "Machine Learning model that aims to predict stock prices using historical data and machine learning techniques. It involves fetching historical stock data, preprocessing it, training a LSTM model, making predictions, and visualizing the results.",
+// //     tags: [
+// //       {
+// //         name: "Python",
+// //         color: "blue-text-gradient",
+// //       },
+// //       {
+// //         name: "Streamlit",
+// //         color: "green-text-gradient",
+// //       },
+// //       {
+// //         name: "Keras",
+// //         color: "pink-text-gradient",
+// //       },
+// //     ],
+// //     image: jobit,
+// //     source_code_link: "https://github.com/anveshas/StockMarketPrediction",
+// //   },
+// //   {
+// //     name: "TrustMed: Where Trust Meets Medication",
+// //     description:
+// //       "Initiated a project to authenticate prescriptions for online medicine purchases, thereby reducing the distribution of illicit and potent medications in the online marketplace.",
+// //     tags: [
+// //       {
+// //         name: "JavaScript",
+// //         color: "blue-text-gradient",
+// //       },
+// //       {
+// //         name: "MySQL",
+// //         color: "green-text-gradient",
+// //       },
+// //       {
+// //         name: "CSS",
+// //         color: "pink-text-gradient",
+// //       },
+// //     ],
+// //     image: tripguide,
+// //     source_code_link: "https://github.com/anveshas/TrustMed",
+// //   },
+// //   {
+// //     name: "Event-Dazzle",
+// //     description:
+// //       "Web application that provides information about all the events happening nearby and allows users to book tickets for these events. Whether it's a concert, movie premiere, seminar, or webinar, Event Dazzle has got you covered.",
+// //     tags: [
+// //       {
+// //         name: "Next.js",
+// //         color: "blue-text-gradient",
+// //       },
+// //       {
+// //         name: "TypeScript",
+// //         color: "green-text-gradient",
+// //       },
+// //       {
+// //         name: "MongoDB",
+// //         color: "pink-text-gradient",
+// //       },
+// //     ],
+// //     image: tripguide,
+// //     source_code_link: "https://github.com/anveshas/Event-Dazzle",
+// //   },
+// //   {
+// //     name: "Tradable",
+// //     description:
+// //       "It is an interactive advisory and analytical software for stocks. It is capable of fetching data about the financials of a company and analyzing live data to indicate high frequency trades.",
+// //     tags: [
+// //       {
+// //         name: "C++",
+// //         color: "blue-text-gradient",
+// //       },
+// //       {
+// //         name: "Algorithms",
+// //         color: "green-text-gradient",
+// //       },
+// //     ],
+// //     image: tripguide,
+// //     source_code_link: "https://github.com/anveshas/Tradable",
+// //   },
+// // ];
+
+// // export { services, technologies, education, experiences, testimonials, projects };
+
 // import {
 //   mobile,
 //   backend,
@@ -34,8 +314,8 @@
 //     title: "About",
 //   },
 //   {
-//     id: "work",
-//     title: "Work",
+//     id: "projects",
+//     title: "Projects",
 //   },
 //   {
 //     id: "contact",
@@ -48,10 +328,10 @@
 //   //   title: "Web Developer",
 //   //   icon: web,
 //   // },
-//   // // {
-//   // //   title: "React Native Developer",
-//   // //   icon: mobile,
-//   // // },
+//   // {
+//   //   title: "React Native Developer",
+//   //   icon: mobile,
+//   // },
 //   // {
 //   //   title: "Frontend Developer",
 //   //   icon: backend,
@@ -254,7 +534,7 @@
 //         color: "pink-text-gradient",
 //       },
 //     ],
-//     image: tripguide,
+//     image: backend,
 //     source_code_link: "https://github.com/anveshas/Event-Dazzle",
 //   },
 //   {
@@ -271,12 +551,13 @@
 //         color: "green-text-gradient",
 //       },
 //     ],
-//     image: tripguide,
+//     image: web,
 //     source_code_link: "https://github.com/anveshas/Tradable",
 //   },
 // ];
 
 // export { services, technologies, education, experiences, testimonials, projects };
+
 
 import {
   mobile,
@@ -327,18 +608,6 @@ const services = [
   // {
   //   title: "Web Developer",
   //   icon: web,
-  // },
-  // {
-  //   title: "React Native Developer",
-  //   icon: mobile,
-  // },
-  // {
-  //   title: "Frontend Developer",
-  //   icon: backend,
-  // },
-  // {
-  //   title: "Problem Solver",
-  //   icon: creator,
   // },
 ];
 
@@ -428,7 +697,7 @@ const education = [
     ],
   },
   {
-    title: "Class XII & Class X, CBSE",
+    title: "Class IX to Class XII, CBSE",
     company_name: "Disha Delphi Public School",
     icon: tesla,
     iconBg: "#383E56",
@@ -437,6 +706,17 @@ const education = [
       "Class X - 91%",
       "Class XII - 90%",
       "I have participated in multiple competitions, excelling in sports such as Badminton and Swimming, as well as in debates. ",
+    ],
+  },
+  {
+    title: "Class I to Class VIII, CBSE",
+    company_name: "Bansal Public School",
+    icon: shopify,
+    iconBg: "#383E56",
+    date: "April 2010 - March 2018",
+    points: [
+      "I have participated in multiple competitions, excelling in sports such as Badminton and Swimming, as well as in debates. ",
+      "I have been part of the prestigious NCC and have been certified with an 'A' certificate.",
     ],
   },
 ];
